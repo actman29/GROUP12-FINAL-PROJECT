20fb142684a7d85287612d1211ad62af8ed6c1fb
Mark Lee <lee00630@algonquinlive.com>
2017-03-29 15:15:13 -0400

    17.3.29 Master changed by Mark

--- app/src/main/java/com/example/marklee/group12_final_project/MainActivity.java
+++ app/src/main/java/com/example/marklee/group12_final_project/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.marklee.group12_final_project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.nav_auto) {
+         // Intent intent = new Intent(MainActivity.this, AutomobileMain.class);
+         //   startActivity(intent);    // Automobile Activity
+
+        } else if(id == R.id.nav_kitchen) {
+
+        } else if(id == R.id.nav_todo) {
+
+        } else if(id == R.id.nav_living) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
